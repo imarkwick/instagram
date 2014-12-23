ab0dcc39306e517adee7da838a2aa2730abe54ce
imarkwick <izzymarkwick@gmail.com>
2014-12-23 18:05:58 +0000

    photo model

--- spec/features/photos_feature_spec.rb
+++ spec/features/photos_feature_spec.rb
@@ -9,4 +9,16 @@ feature 'photos' do
 			expect(page).to have_link 'Add photo'
 		end
 	end
+
+	context 'photos have been added' do
+		before do
+			Photo.create(picture: 'img_url')
+		end
+
+		scenario 'displays photos' do
+			visit '/photos'
+			expect(page).to have_content('img_url')
+			expect(page).not_to have_content('No photos')
+		end
+	end
 end
